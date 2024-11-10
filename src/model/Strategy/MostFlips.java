f732ba6038e26cb69f0e6af2425f207e8d2385cf
NateGPTs <n8104n@gmail.com>
2024-11-10 17:03:02 -0500

    2nd

--- src/model/Strategy/MostFlips.java
+++ src/model/Strategy/MostFlips.java
@@ -1,7 +1,20 @@
 package model.Strategy;
 
+import java.util.HashMap;
+import model.ReadOnlyThreeThriosModel;
+import model.card.Card;
+import model.player.Player;
+
+/**
+ * Represents a strategy for the CPU, where moves that flip the most cards are "best".
+ */
 public class MostFlips implements ThreeTriosStrategy {
 
-  public
+  public HashMap<Coordinate, Card> chooseMove(ReadOnlyThreeThriosModel model, Player player) {
+
+
+
+
+  }
 
 }
--- src/model/Strategy/ThreeTriosStrategy.java
+++ src/model/Strategy/ThreeTriosStrategy.java
@@ -1,8 +1,22 @@
 package model.Strategy;
 
+import java.util.HashMap;
+import model.ReadOnlyThreeThriosModel;
+import model.card.Card;
+import model.player.Player;
+
+/**
+ * Represents a play style cpus can use for the game.
+ */
 public interface ThreeTriosStrategy {
 
-
-
+  /**
+   * Outputs best moves for the CPU, represented by coordinate to card pairings in a hashmap.
+   *
+   * @param model The model given.
+   * @param player the player doing the move.
+   * @return coordinate to card pairings represented by a hashmap.
+   */
+  public HashMap<Coordinate, Card> chooseMove(ReadOnlyThreeThriosModel model, Player player);
 
 }
